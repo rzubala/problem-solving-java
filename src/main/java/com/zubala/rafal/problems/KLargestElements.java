8c5da1959a7492601691ac44b1bedacb349d245b
Rafał Zubala <rzubala@gmail.com>
2023-12-18 14:06:51 +0100

    k largest binary search

--- src/main/java/com/zubala/rafal/problems/KLargestElements.java
+++ src/main/java/com/zubala/rafal/problems/KLargestElements.java
@@ -2,13 +2,37 @@ package com.zubala.rafal.problems;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
+import java.util.stream.Collectors;
 
 /**
  * @see <a href="https://www.geeksforgeeks.org/k-largestor-smallest-elements-in-an-array/">Problem description</a>
  */
 public class KLargestElements {
-    public Integer[] kLargest(Integer[] arr, int k) {
+    public Integer[] kLargestSort(Integer[] arr, int k) {
         Arrays.sort(arr, Collections.reverseOrder());
         return Arrays.stream(arr).limit(k).toList().toArray(new Integer[0]);
+    }
+
+    private int findKLargest(Integer[] arr, int k) {
+        int low = Arrays.stream(arr).mapToInt(Integer::intValue).min().getAsInt();
+        int high = Arrays.stream(arr).mapToInt(Integer::intValue).max().getAsInt();
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            long count = Arrays.stream(arr).filter(i -> i > mid).count();
+            if (count >= k) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return high;
+    }
+
+    public Integer[] kLargestBinarySearch(Integer[] arr, int k) {
+        int kthLargest = findKLargest(arr, k);
+        Arrays.sort(arr, Collections.reverseOrder());
+        return Arrays.stream(arr).filter(i -> i >= kthLargest).toList().toArray(new Integer[0]);
     }
 }
--- src/test/java/com/zubala/rafal/problems/KLargestElementsTest.java
+++ src/test/java/com/zubala/rafal/problems/KLargestElementsTest.java
@@ -13,11 +13,24 @@ class KLargestElementsTest {
         Integer[] input1 = {1, 23, 12, 9, 30, 2, 50};
         Integer[] expected1 = {50, 30, 23};
         int k = 3;
-        assertArrayEquals(expected1, kLargestElements.kLargest(input1, k));
+        assertArrayEquals(expected1, kLargestElements.kLargestSort(input1, k));
 
         Integer[] input2 = {11, 5, 12, 9, 44, 17, 2};
         Integer[] expected2 = {44, 17};
         k = 2;
-        assertArrayEquals(expected2, kLargestElements.kLargest(input2, k));
+        assertArrayEquals(expected2, kLargestElements.kLargestSort(input2, k));
+    }
+
+    @Test
+    void kLargestBinarySearch() {
+        Integer[] input1 = {1, 23, 12, 9, 30, 2, 50};
+        Integer[] expected1 = {50, 30, 23};
+        int k = 3;
+        assertArrayEquals(expected1, kLargestElements.kLargestBinarySearch(input1, k));
+
+        Integer[] input2 = {11, 5, 12, 9, 44, 17, 2};
+        Integer[] expected2 = {44, 17};
+        k = 2;
+        assertArrayEquals(expected2, kLargestElements.kLargestBinarySearch(input2, k));
     }
 }

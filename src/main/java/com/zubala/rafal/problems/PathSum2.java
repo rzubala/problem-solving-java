42d9facbe2a3e64dd55fbcb026a38833d3cbe592
Rafał Zubala <rzubala@gmail.com>
2024-01-04 12:23:09 +0100

    PathSum2: initial

---
+++ src/main/java/com/zubala/rafal/problems/PathSum2.java
@@ -0,0 +1,14 @@
+package com.zubala.rafal.problems;
+
+import com.zubala.rafal.problems.model.TreeNode;
+
+import java.util.List;
+
+/**
+ * @see <a href="https://leetcode.com/problems/path-sum-ii/description/">Problem description</a>
+ */
+public class PathSum2 {
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        return null;
+    }
+}
---
+++ src/test/java/com/zubala/rafal/problems/PathSum2Test.java
@@ -0,0 +1,30 @@
+package com.zubala.rafal.problems;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static com.zubala.rafal.problems.utils.TreeNodeUtils.buildBinaryTree;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PathSum2Test {
+    private final PathSum2 pathSum2 = new PathSum2();
+    @Test
+    public void testPathSum2() {
+        Integer[] input1 = { 5,4,8,11,null,13,4,7,2,null,null,null,1 };
+        List<List<Integer>> result1 = pathSum2.pathSum(buildBinaryTree(input1), 22);
+        assertNotNull(result1);
+        assertEquals(result1.size(), 2);
+        Integer[] expected1_1 = {5,4,11,2};
+        Integer[] expected1_2 = {5,8,4,5};
+        assertArrayEquals(expected1_1, result1.get(0).toArray());
+        assertArrayEquals(expected1_2, result1.get(1).toArray());
+
+
+        Integer[] input2 = { 1,2,3 };
+        List<List<Integer>> result2 = pathSum2.pathSum(buildBinaryTree(input2), 5);
+        assertNotNull(result2);
+        assertTrue(result2.isEmpty());
+    }
+
+}

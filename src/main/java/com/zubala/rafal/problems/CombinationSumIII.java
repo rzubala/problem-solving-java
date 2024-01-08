3bc1000ad47af0c57af4f36c095e9cc3b79157b4
Rafał Zubala <rzubala@gmail.com>
2024-01-08 15:42:01 +0100

    CombinationSumIII: solution

--- src/main/java/com/zubala/rafal/problems/CombinationSumIII.java
+++ src/main/java/com/zubala/rafal/problems/CombinationSumIII.java
@@ -7,8 +7,29 @@ import java.util.List;
  * @see <a href="https://leetcode.com/problems/combination-sum-iii/description/">Problem description</a>
  */
 public class CombinationSumIII {
+
+    private final static int FROM = 1;
+
+    private final static int TO = 9;
+
     public List<List<Integer>> combinationSum3(int k, int n) {
         List<List<Integer>> result = new LinkedList<>();
+        backtrack(k, n, FROM, new LinkedList<Integer>(), result);
         return result;
+    }
+
+    private void backtrack(int k, int target, int start, LinkedList<Integer> tmpList, List<List<Integer>> result) {
+        if (tmpList.size() == k && target == 0) {
+            result.add(new LinkedList<>(tmpList));
+            return;
+        }
+        if (target <= 0) {
+            return;
+        }
+        for (int i=start;i<=TO;i++) {
+            tmpList.add(i);
+            backtrack(k, target - i, i + 1, tmpList, result);
+            tmpList.removeLast();
+        }
     }
 }
--- src/test/java/com/zubala/rafal/problems/CombinationSumIIITest.java
+++ src/test/java/com/zubala/rafal/problems/CombinationSumIIITest.java
@@ -3,6 +3,7 @@ package com.zubala.rafal.problems;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -19,16 +20,22 @@ class CombinationSumIIITest {
 
         List<List<Integer>> result2 = combinationSumIII.combinationSum3(3, 9);
         assertEquals(3, result2.size());
+
         Integer[] result2_1 = {1,2,6};
         assertArrayEquals(result2_1, result2.get(0).toArray());
 
         Integer[] result2_2 = {1,3,5};
-        assertArrayEquals(result2_2, result2.get(0).toArray());
+        assertArrayEquals(result2_2, result2.get(1).toArray());
 
         Integer[] result2_3 = {2,3,4};
-        assertArrayEquals(result2_3, result2.get(0).toArray());
+        assertArrayEquals(result2_3, result2.get(2).toArray());
 
         List<List<Integer>> result3 = combinationSumIII.combinationSum3(4, 1);
         assertEquals(0, result3.size());
+
+        List<List<Integer>> result4 = combinationSumIII.combinationSum3(9, 45);
+        assertEquals(1, result4.size());
+        Integer[] result4_1 = {1,2,3,4,5,6,7,8,9};
+        assertArrayEquals(result4_1, result4.get(0).toArray());
     }
 }
